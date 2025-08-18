03b0cf9301e8ee27fa08486a983f4805306945ce
Mauvis Ledford <switchstatement@gmail.com>
2025-08-18 10:19:23 +0000

    cleanup

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -9,15 +9,12 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3001
+      PORT: 3008
     },
     env_development: {
       NODE_ENV: 'development',
-      PORT: 3001
+      PORT: 3008
     },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
     time: true,
     merge_logs: true,
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
